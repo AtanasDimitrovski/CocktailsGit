f4e9daf50f6841e0648b1fb9a8f137f6046e7974
mStamenkovik <m.stamenkovik@gmail.com>
2014-08-24 18:28:10 +0200

    sliki kategorii

--- cocktails/application/views/main.php
+++ cocktails/application/views/main.php
@@ -27,21 +27,22 @@
        <div id="strawberry-cocktail">
        </div>
        <div id="cocktail-categories">
-           <div></div>
-           <div></div>
-           <div></div>
-           <div></div>
-           <div></div>
+           <div class="box first-box"><span class="text-category">Tequilla</br> Based </br>Cocktails</span><img height="180" width="180" src="css/images/tequllaSunrise001.png" style=" position: absolute; padding-left: 110px;"></div>
+           <div class="box second-box"><span class="text-category">Vodka</br> Based </br>Cocktails</span><img height="180" width="135" src="css/images/black-russian.png" style=" position: absolute; padding-left: 144px;"></div>
+           <div class="box third-box"><span class="text-category">Whiskey</br> Based </br>Cocktails</span><img height="180" width="135" src="css/images/old-fashioned001.png" style=" position: absolute; padding-left: 143px;"></div>
+           <div class="box fourth-box"><span class="text-category">Champagne</br> Based </br>Cocktails</span><img height="173" width="135" src="css/images/mimosa.png" style=" position: absolute; padding-left: 143px;"></div>
+           <div class="box fifth-box"><span class="text-category">Gin</br> Based </br>Cocktails</span><img height="180" width="135" src="css/images/gimlet001.png" style=" position: absolute; padding-left: 129px;"></div>
        </div>
    </div>  
    <!-- Menu cocktail -->  
      <div id="cocktails-by-letter" >
         <span style="padding-left: 35px; font-size: 20px; color: white;">Choose a cocktail:</span>
         <ul class="letters">
-         <?php for($i=65; $i <= 90; $i++){ ?> 
-         	 <li class="letter-link"><a href="google.com"><?php print(chr($i))?> </a></li> | 
+         <?php for($i=65; $i < 90; $i++){ ?> 
+         	 <li class="letter-link"><a href="http:\\google.com"><?php print(chr($i))?></a></li> <span style="color: white">|</span>
          	<?php 
          }?>
+          <li class="letter-link"><a href="http:\\google.com">Z</a></li>
          </ul>
      </div>
      
